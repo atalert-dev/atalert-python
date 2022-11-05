54ce07926865c10bf7808cb230c1e92e653e22a9
Kevin <kevin@splatcollision.com>
2022-11-04 20:07:38 -0400

    adds more shipyard environment variables for display purposes

--- atalert/shipyard.py
+++ atalert/shipyard.py
@@ -8,7 +8,12 @@ def check():
 		"from_shipyard": True,
 		"org_name": os.environ.get('SHIPYARD_ORG_NAME'),
 		"project_id": os.environ.get('SHIPYARD_PROJECT_ID'),
+		"project_name": os.environ.get('SHIPYARD_PROJECT_NAME'),
 		"vessel_id": os.environ.get('SHIPYARD_VESSEL_ID'),
+		"vessel_name": os.environ.get('SHIPYARD_VESSEL_NAME'),
+		"start_time": os.environ.get('SHIPYARD_VESSEL_START_TIME'),
+		"fleet_id": os.environ.get('SHIPYARD_FLEET_ID'),
+		"fleet_name": os.environ.get('SHIPYARD_FLEET_NAME'),
 		"log_id": os.environ.get('SHIPYARD_LOG_ID'),
 	}
 	if vals['project_id'] and vals['vessel_id'] and vals['log_id']:
--- tests/test_shipyard.py
+++ tests/test_shipyard.py
@@ -10,6 +10,10 @@ def test_shipyard_meta(slug):
 	os.environ['SHIPYARD_PROJECT_ID'] = 'testprojectid'
 	os.environ['SHIPYARD_VESSEL_ID'] = 'testvesselid'
 	os.environ['SHIPYARD_LOG_ID'] = 'testlogid'
+	os.environ['SHIPYARD_PROJECT_NAME'] = 'project name'
+	os.environ['SHIPYARD_VESSEL_NAME'] = 'vessel name'
+	os.environ['SHIPYARD_FLEET_NAME'] = 'fleet name'
+	os.environ['SHIPYARD_VESSEL_START_TIME'] = '2021-03-12T21:22:47Z'
 	
 	resp = atalert.ok(slug, {'data': 'shipyard fake data'}, template=AlertTemplates.json)
 	
